e0b15759d478fbd80f34ece3e8e0628b9a889f3e
Andrei Serea <andrei.serea@gmail.com>
2013-05-27 15:21:33 +0300

    support for arquillian

---
+++ jbehave-core/src/main/java/org/jbehave/core/steps/SimpleStepLifecycleObserver.java
@@ -0,0 +1,13 @@
+package org.jbehave.core.steps;
+
+public class SimpleStepLifecycleObserver implements StepLifecycleObserver {
+
+	public void beforeStepExecution(Step step) {
+		//do nothing
+	}
+
+	public void afterStepExecution(Step step) {
+		//do nothing
+	}
+
+}
